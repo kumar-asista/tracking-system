221af1e7909a1f7628e1a51af9f7676a777047cd
saadmohammed <jmsaad46@gmail.com>
2018-10-13 16:55:12 +0530

    locations rest service

--- src/main/java/com/tracking_system/model/Locations.java
+++ src/main/java/com/tracking_system/model/Locations.java
@@ -5,38 +5,61 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "locations")
-public class Locations {
+public class Locations extends Audit{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "loc_id")
-    private Integer locId;
+    private Integer locationid;
     @Column(name = "latitude")
-    private Double Latitude;
+    private Double latitude;
     @Column(name = "longitude")
-    private Double Longitude;
-    @Column (name = "rootid")
+    private Double longitude;
+    @Column(name = "root_id")
     private Integer rootid;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "rootid", referencedColumnName = "root_id", insertable = false, updatable = false)
-    private Root root;
 
+    /*@ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "root_id", referencedColumnName = "root_id", insertable = false, updatable = false)
+    private Root root;
     public Root getRoot() {
         return root;
     }
-
     public void setRoot(Root root) {
         this.root = root;
-    }
+    }*/
 
     public Locations() {
     }
-
-    public Locations(Integer locId, Double latitude, Double longitude, Integer rootid) {
-        this.locId = locId;
-        Latitude = latitude;
-        Longitude = longitude;
+    public Locations(Integer locationid, Double latitude, Double longitude, Integer rootid) {
+        this.locationid = locationid;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.rootid = rootid;
+    }
+
+    public Integer getLocationid() {
+        return locationid;
+    }
+
+    public void setLocationid(Integer locationid) {
+        this.locationid = locationid;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 
     public Integer getRootid() {
@@ -46,29 +69,4 @@ public class Locations {
     public void setRootid(Integer rootid) {
         this.rootid = rootid;
     }
-
-    public Integer getLocId() {
-        return locId;
-    }
-
-    public void setLocId(Integer locId) {
-        this.locId = locId;
-    }
-
-    public Double getLatitude() {
-        return Latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        Latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return Longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        Longitude = longitude;
-    }
-
 }
